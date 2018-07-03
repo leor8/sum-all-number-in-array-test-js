53f5f3e04999744c4f598dde63eea5bf8685bf42
leor8 <leo442183205@gmail.com>
2018-07-03 21:54:51 +0000

    test passed

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+let anArr = [[1, 2, [[3], 4]], 5, []];
+
+
 function sumItems(array) {
+  let sum = 0;
   // Sum all the numbers in the array
+  for (item of array) {
+    if(Array.isArray(item)){
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+
+  return sum;
 }
+
+// sumItems(anArr);
+console.log(sumItems(anArr));
 
 module.exports = sumItems;
